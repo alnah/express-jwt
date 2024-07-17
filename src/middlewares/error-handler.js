614d187aaecd6cf16a958cbad21e38a194617aed
alnah <alexis.nahan@gmail.com>
2024-07-17 20:41:03 -0300

    feat(error-handler): use custom error api

--- src/middlewares/error-handler.js
+++ src/middlewares/error-handler.js
@@ -1,9 +1,14 @@
-const errorHandlerMiddleware = (err, _, res) => {
-  console.error(err);
-  res.status(500).json({
-    message: "An internal server error occurred",
-    error: err.message,
-  });
+const { CustomApiError } = require("../errors/custom-error");
+
+const errorHandlerMiddleware = (err, req, res, next) => {
+  if (err instanceof CustomApiError) {
+    res.status(err.statusCode).json({ message: err.message });
+  } else {
+    res.status(500).json({
+      message: "An internal server error occurred",
+      error: err.message || "Unknown error",
+    });
+  }
 };
 
 module.exports = errorHandlerMiddleware;
