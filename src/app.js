105a4594f7ac98f63a61ea82ba38d1304778fd32
alnah <alexis.nahan@gmail.com>
2024-07-17 18:25:32 -0300

    feat(app): use error handler middleware

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ require("dotenv").config();
 const express = require("express");
 
 const routeNotFoundMiddleware = require("./middlewares/route-not-found");
+const errorHandlerMiddleware = require("./middlewares/error-handler");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(routeNotFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 app.listen(3000, () => console.log(`Server is listening on port ${port}`));
