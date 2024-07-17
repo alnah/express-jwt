0bcb4c512fbf5a5999d4ad2c83fed524556e1040
alnah <alexis.nahan@gmail.com>
2024-07-17 20:43:22 -0300

    style(error-handler): disable eslint error

--- src/middlewares/error-handler.js
+++ src/middlewares/error-handler.js
@@ -1,5 +1,7 @@
 const { CustomApiError } = require("../errors/custom-error");
 
+// next is used by express-async-errors
+// eslint-disable-next-line no-unused-vars
 const errorHandlerMiddleware = (err, req, res, next) => {
   if (err instanceof CustomApiError) {
     res.status(err.statusCode).json({ message: err.message });
