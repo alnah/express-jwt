e11c73f856f36395d9cf3fa140af446fc925452d
alnah <alexis.nahan@gmail.com>
2024-07-17 18:23:14 -0300

    feat(app): use route not found middleware

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 
+const routeNotFoundMiddleware = require("./middlewares/route-not-found");
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(routeNotFoundMiddleware);
+
 app.listen(3000, () => console.log(`Server is listening on port ${port}`));
