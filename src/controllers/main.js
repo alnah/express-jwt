d63ebf96b62328d1878d4a78d4db8ec1249a9ad0
alnah <alexis.nahan@gmail.com>
2024-07-18 05:58:10 -0300

    feat(login): add json web token

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -1,11 +1,20 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const { v4: uuid } = require("uuid");
+
 const { CustomApiError } = require("../errors/custom-error");
+
+const jwtSecret = process.env.JWT_SECRET;
 
 const login = async (req, res) => {
   const { username, password } = req.body;
-  if (!username || !password) {
+  if (!username || !password)
     throw new CustomApiError("Please provide a username or a password", 400);
-  }
-  res.send("Fake Login/register/Signup Route");
+  // use for demonstration, normally provided by the database
+  const id = uuid();
+  // try to keep the payload small, better experience for user
+  const token = jwt.sign({ id, username }, jwtSecret, { expiresIn: "30d" });
+  res.send({ message: "user created", token });
 };
 
 const dashboard = async (_, res) => {
